fbd193accf348270da91b4017f650015f821bfb5
Ana Castedo <anaccastedo@gmail.com>
2023-04-20 12:01:37 +0100

    middle step with redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,31 @@
 import classes from './Counter.module.css';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+    const dispatch = useDispatch();
+
+    const counter = useSelector((state) => state.counter);
+    const show = useSelector((state) => state.showCounter);
+
+    const incrementHandler = () => {
+        dispatch({type:'INC_COUNTER'});
+    }
+    const decrementHandler = () => {
+        dispatch({type:'DEC_COUNTER'});
+    }
+    const toggleCounterHandler = () => {
+        // dispatch({type:'TOGGLE_COUNTER'});
+    }
+
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+        <div>
+            <button onClick={incrementHandler}>Increment</button>
+            <button onClick={decrementHandler}>Decrement</button>
+        </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
---
+++ src/store/index.js
@@ -0,0 +1,56 @@
+import {createStore} from 'redux';
+
+
+const initialState={
+    counter:0
+}
+
+// Reducer
+const counterReducer=(state=initialState,action)=>{
+    if(action.type==='INC_COUNTER'){
+        return{
+            ...state,
+            counter:state.counter+1
+        }
+    }
+    if(action.type==='DEC_COUNTER'){
+        return{
+            ...state,
+            counter:state.counter-1
+        }
+    }
+    return state;
+}
+
+// Store
+const store=createStore(counterReducer);
+console.log(store.getState());
+
+// Subscription
+store.subscribe(()=>{
+    console.log('[Subscription]',store.getState());
+}
+)
+
+
+export default store;
+
+
+// Redux Middleware
+//
+//
+// Redux middleware provides a third-party extension point between dispatching an action, and the moment it reaches the reducer.
+//
+//     Middleware is a function that takes an action and either dispatches another action or does nothing.
+//
+//     It is a way to extend Redux with custom functionality.
+//
+//     It lets you wrap the store’s dispatch method for fun and profit.
+//
+//     It is a way to intercept and act on actions that have been dispatched to the store.
+//
+//     It is a way to do asynchronous actions like AJAX calls.
+//
+//     It is a way to do logging, crash reporting, talking to an asynchronous API, routing, and more.
+
+
